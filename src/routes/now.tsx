78f50a2879bf7be988297be2f549a5ca2c2a4f99
Nikita Voloboev <nikita.voloboev@gmail.com>
2024-01-04 11:56:16 +0300

    .

--- src/routes/now.tsx
+++ src/routes/now.tsx
@@ -1,25 +1,12 @@
-import { A } from "@solidjs/router"
 import Nav from "~/components/Nav"
 import PageWrap from "~/components/PageWrap"
+import NowContent from "~/mdx/now.md"
 
 export default function Now() {
   return (
     <PageWrap>
       <Nav activePage={"Now"} />
-      <p>
-        <A href="../projects">Building ideas</A> &amp; completing my{" "}
-        <a href="https://wiki.nikiv.dev/focusing/goals">goals</a> to live a{" "}
-        <a href="https://wiki.nikiv.dev/future/">better future</a>.
-      </p>
-      <p>
-        Writing monthly{" "}
-        <a href="https://wiki.nikiv.dev/looking-back/">look backs</a> reflecting
-        on what I worked &amp; thought about.
-      </p>
-      {/* <p>
-        Currently spending time in Saint Petersburg.{" "}
-        <a href="https://twitter.com/nikitavoloboev">Say 👋</a>.
-      </p> */}
+      <NowContent />
     </PageWrap>
   )
 }
