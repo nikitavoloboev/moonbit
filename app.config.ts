b73b3d2af7ac8338181f35e7de99a45d6f43fc51
Nikita <nikita.voloboev@gmail.com>
2024-03-16 17:03:50 +0400

    .

---
+++ app.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from "@solidjs/start/config"
+/* @ts-ignore */
+import mdx from "@vinxi/plugin-mdx"
+
+export default defineConfig({})
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,26 @@
 import { defineConfig } from "@solidjs/start/config"
-// mdx code taken from https://github.com/solidjs/solid-start/tree/main/examples/with-mdx
-/* @ts-ignore */
+// @ts-ignore
 import pkg from "@vinxi/plugin-mdx"
 
 const { default: mdx } = pkg
 
-// https://nitro.unjs.io/deploy/providers/cloudflare deploy on cloudflare pages
-// using https://ryanjc.com/blog/solidstart-cloudflare-pages/ to deploy solid start 2
-
-// TODO: add ssr to the website
-// add https://github.com/cyco130/vite-plugin-mdx (open issue https://discord.com/channels/722131463138705510/910635844119982080/1190950777880060037)
 export default defineConfig({
-  start: {
-    ssr: false,
-    extensions: ["mdx", "md"],
-    server: {
-      preset: "cloudflare_pages",
-      // enable CF Pages node compatiblity https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
-      rollupConfig: {
-        external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
-      },
+  ssr: false,
+  extensions: ["mdx", "md"],
+  server: {
+    preset: "cloudflare_pages",
+    // enable CF Pages node compatiblity https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
+    rollupConfig: {
+      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
     },
   },
-  plugins: [
-    mdx.withImports({})({
-      jsx: true,
-      jsxImportSource: "solid-js",
-      providerImportSource: "solid-mdx",
-    }),
-  ],
+  vite: {
+    plugins: [
+      mdx.withImports({})({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+      }),
+    ],
+  },
 })
